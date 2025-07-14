a49f989b29c45d0103ce61ea725bffc90926795f
HassanAbbas107 <qiqbx223@gmail.com>
2025-07-14 08:43:56 +0300

    add reload

--- script/app.js
+++ script/app.js
@@ -1,4 +1,5 @@
 function init() {
+const message = document.querySelector("#message")
 const blocks = document.querySelectorAll(".block")
 // let randomBox=Array.from({length: 9}, () => Math.floor(Math.random() * 9));//1st random method
 let blockIndex=[0,1,2,3,4,5,6,7,8]
@@ -42,7 +43,11 @@ let gameOver=false
     blocks.forEach(block=> {
         block.addEventListener('click',clickFun)
     })
-
+    function lose(){
+    //     blocks.forEach(block => {
+    //         block.removeEventListener()
+    //     });
+    }
     function clickFun(event) {
          blockIndex = NewArr[event.target.id]
          checkForWin(event)
@@ -58,15 +63,26 @@ let gameOver=false
             console.log("This is correct")
             const list = event.target.classList;
             list.add("myStyle");
-        }else{
-            
+        } else {
             const list2 = event.target.classList;
-            
             list2.add("myStyle2");
+
+            blocks.forEach((block)=>{
+                block.addEventListener('click',()=>{
+
+                })
+            })
+            message.textContent = "YOU LOSE :("
+            setTimeout(() => {
+                location.reload()
+            }, 2000);
         }
     }
+
     function play(){
         showNumber()
         setTimeout(hide, 3000)
-    }play()
+
+    }
+    play()
     }document.addEventListener("DOMContentLoaded", init);
